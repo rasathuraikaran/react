28d899f28e58d5c2a32f3781329d538a4158045a
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2025-03-09 15:29:47 +0530

    added state with object

--- src/components/Schooter.js
+++ src/components/Schooter.js
@@ -1,23 +1,22 @@
 import { useState } from "react";
 
 function Schooter() {
-   const  [color,setColor]= useState("Red");
-   const  [brand,setBrand]= useState("Tesla");
-
-   const  [model,setModel]= useState("1X2000");
-
-   const  [year,setYearr]= useState("2025");
-
+ const [Schooter, setSchooter] = useState({
+  Color: "Red",
+  Brand: "Tesla",
+  Model: "1X200",
+  Year: "2000",
+ });
 
  return (
   <div>
    <h1>My schooter</h1>
-   <p>Color : {color}</p>
-   <p>Brand : {brand}</p>
-   <p>Model : {model}</p>
-   <p>Year : {year}</p>
+   <p>Color : {Schooter.Color}</p>
+   <p>Brand : {Schooter.Brand}</p>
+   <p>Model : {Schooter.Model}</p>
+   <p>Year : {Schooter.Year}</p>
   </div>
  );
 }
 
-export default Schooter
+export default Schooter;
