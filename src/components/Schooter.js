5de379a29c6a7b4c62fc3eb909d72bec986e6023
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2025-03-19 06:19:15 +0530

    -keep the prevous statte

--- src/components/Schooter.js
+++ src/components/Schooter.js
@@ -8,6 +8,12 @@ function Schooter() {
   Year: "2000",
  });
 
+ function updatecolor() {
+  return setSchooter((previousState) => {
+   return { ...previousState, Color: "white" };
+  });
+ }
+
  return (
   <div>
    <h1>My schooter</h1>
@@ -15,6 +21,7 @@ function Schooter() {
    <p>Brand : {Schooter.Brand}</p>
    <p>Model : {Schooter.Model}</p>
    <p>Year : {Schooter.Year}</p>
+   <button onClick={updatecolor}>change color</button>
   </div>
  );
 }
