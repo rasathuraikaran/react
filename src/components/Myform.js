9f24c76d74c0fe7108202374afced97afa8dcd8d
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2025-03-23 13:19:45 +0530

    submitting form without brower refresh

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
 function Myform() {
  const [name, setName] = useState("");
+ const [count, setCount] = useState(0);
+ function handleSub(e) {
+  e.preventDefault();
+  setCount((previousState) => {
+   return previousState + 1;
+  });
+ }
  return (
-  <form>
+  <form onSubmit={handleSub}>
    <label>
     {" "}
     Enter your name :{" "}
     <input type="text" onChange={(event) => setName(event.target.value)} />
-   </label>
+   </label>{" "}
+   <br />
+   <input type="submit" value="Submit form " />
+   <br />
+   <label htmlFor="">Count {count}</label>
+   <br />
    <label htmlFor="">The name is {name}</label>
   </form>
  );
