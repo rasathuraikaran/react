628f682a4dba834b1d7bdf81509c86854638c216
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2025-03-23 13:05:33 +0530

    getting input value to State

--- src/components/Myform.js
+++ src/components/Myform.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 function Myform() {
+ const [name, setName] = useState("");
  return (
   <form>
    <label>
     {" "}
-    Enter your name : <input type="text" />
+    Enter your name :{" "}
+    <input type="text" onChange={(event) => setName(event.target.value)} />
    </label>
+   <label htmlFor="">The name is {name}</label>
   </form>
  );
 }
